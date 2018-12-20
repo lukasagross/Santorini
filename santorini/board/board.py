cea958b58a18c52704aca2fa378fb564475d80a6
lukas <lukasgross2020@u.northwestern.edu>
2018-12-20 16:56:21 -0600

    [BOARD] Add init function and tests

--- santorini/board/board.py
+++ santorini/board/board.py
@@ -1,3 +1,73 @@
 """
 Class representing a Santorini board
 """
+
+
+class BoardError(Exception):
+    """
+    Exception raised by the Board class when building invalid boards
+    """
+
+
+class Board:
+    """
+    Class representing a Santorini board
+    """
+    LENGTH = 5
+    WIDTH = 5
+    MAX_HEIGHT = 4
+    MAX_WORKERS = 3
+    COLORS = ["white", "blue"]
+
+    def __init__(self, board):
+        if len(board) != Board.WIDTH or any(len(row) != Board.LENGTH for row in board):
+            raise BoardError(f"Board dimensions must be {Board.LENGTH}x{Board.WIDTH}")
+
+        flat_board = [cell for row in board for cell in row]
+
+        if any(not (isinstance(cell, (int, list))) for cell in flat_board):
+            raise BoardError(f"Cells must be int or list[int str]")
+
+        if any(cell > Board.MAX_HEIGHT or cell < 0 for cell in flat_board if isinstance(cell, int)):
+            raise BoardError(f"No cell can have height >{Board.MAX_HEIGHT} or <0")
+
+        workers = set()
+
+        for cell in filter(lambda cell: isinstance(cell, list), flat_board):
+            if len(cell) != 2 or not (isinstance(cell[0], int) and isinstance(cell[1], str)):
+                raise BoardError(f"Occupied cells must be list[int str], not {cell}")
+
+            height, worker = cell
+
+            if worker in workers:
+                raise BoardError(f"Workers must be unique, {worker} was found twice")
+
+            if height > Board.MAX_HEIGHT or height < 0:
+                raise BoardError(f"No cell can have height > {Board.MAX_HEIGHT} or <0")
+
+            if not any(worker == color + str(num)
+                       for color in Board.COLORS
+                       for num in range(1, 1 + Board.MAX_WORKERS)):
+                raise BoardError(f"Workers must have format [color][number], not {worker}")
+
+            workers.add(worker)
+
+        self.board = board
+
+    def place_worker(self, worker, pos):
+        """
+        Create a new board with a new worker at pos
+        :param worker: string name of the worker
+        :param pos: list[int int] position to place the worker
+        :return: Board with the new worker at pos
+        :raise: BoardError if pos is an occupied cell or does not exist
+        """
+
+    def move_worker(self, worker, direction):
+        """
+        Create a new board with a worker moved one cell over
+        :param worker: string name of the worker
+        :param direction: string name of the direction
+        :return: Board with the worker moved one unit
+        :raise: BoardError if the destination cell does not exist
+        """
--- santorini/tests/board/test_board.py
+++ santorini/tests/board/test_board.py
@@ -1,1 +1,162 @@
-import santorini.board.board
+import pytest
+
+from santorini.board.board import Board, BoardError
+
+
+def test_valid_init():
+    board1 = [[0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0]]
+
+    assert Board(board1).board == board1
+
+    board2 = [[0, 0, 0, 0, 0],
+              [0, 3, 0, 0, 2],
+              [0, 0, 1, 0, 0],
+              [1, 0, 0, 1, 0],
+              [0, 0, 4, 0, 0]]
+
+    assert Board(board2).board == board2
+
+    board3 = [[0, [2, "white1"], 0, 0, 0],
+              [0, 3, 0, 0, [2, "blue2"]],
+              [0, 0, [1, "blue1"], 0, 0],
+              [[1, "white2"], 0, 0, 1, 0],
+              [0, 0, 4, 0, 0]]
+
+    assert Board(board3).board == board3
+
+
+def test_invalid_init():
+    board1 = [[0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0]]
+
+    with pytest.raises(BoardError):
+        # Wrong Width
+        assert Board(board1).board == board1
+
+    board2 = [[0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0]]
+
+    with pytest.raises(BoardError):
+        # Wrong length
+        assert Board(board2).board == board2
+
+    board3 = [[0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 5, 0],
+              [0, 0, 0, 0, 0]]
+
+    with pytest.raises(BoardError):
+        # Height larger than max height
+        assert Board(board3).board == board3
+
+    board4 = [[0, 0, 0, 0, 0],
+              [0, 0, 0, 0, "blue1"],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0]]
+
+    with pytest.raises(BoardError):
+        # Cell not instance of int
+        assert Board(board4).board == board4
+
+    board5 = [[0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [[0, "white1", "white2"], 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0]]
+
+    with pytest.raises(BoardError):
+        # Occupied cell list has length 3
+        assert Board(board5).board == board5
+
+    board6 = [[0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [["blue1", "white1"], 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0]]
+
+    with pytest.raises(BoardError):
+        # Occupied cell height is not int
+        assert Board(board6).board == board6
+
+    board7 = [[0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [[0, 0], 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0]]
+
+    with pytest.raises(BoardError):
+        # Occupied cell worker is not str
+        assert Board(board7).board == board7
+
+    board8 = [[0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [-1, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0]]
+
+    with pytest.raises(BoardError):
+        # Cell has negative height
+        assert Board(board8).board == board8
+
+    board9 = [[0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [[-1, "white1"], 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0]]
+
+    with pytest.raises(BoardError):
+        # Occupied cell has negative height
+        assert Board(board9).board == board9
+
+    board10 = [[0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0],
+               [[0, "red1"], 0, 0, 0, 0],
+               [0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0]]
+
+    with pytest.raises(BoardError):
+        # Occupied cell has invalid worker
+        assert Board(board10).board == board10
+
+    board11 = [[0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0],
+               [[0, "blue0"], 0, 0, 0, 0],
+               [0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0]]
+
+    with pytest.raises(BoardError):
+        # Occupied cell has invalid worker
+        assert Board(board11).board == board11
+
+    board12 = [[0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0],
+               [[0, "blue4"], 0, 0, 0, 0],
+               [0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0]]
+
+    with pytest.raises(BoardError):
+        # Occupied cell has invalid worker
+        assert Board(board12).board == board12
+
+    board13 = [[0, 0, 0, 0, 0],
+               [[0, "blue1"], 0, 0, 0, 0],
+               [[0, "blue1"], 0, 0, 0, 0],
+               [0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0]]
+
+    with pytest.raises(BoardError):
+        # Duplicated worker
+        assert Board(board13).board == board13
